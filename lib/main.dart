b6c52cad5418775b90da6bc565ecea9d160bb05b
Andy Ang <andy_ang94@hotmail.com>
2023-02-21 16:31:57 +0800

    - add reset quiz

--- lib/main.dart
+++ lib/main.dart
@@ -13,27 +13,53 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  var _questionIndex = 0;
-
   final _questions = const [
     {
       'questionText': 'What\'s your favourite color?',
-      'answers': ['Black', 'Red', 'Green', 'White']
+      'answers': [
+        {'text': 'Black', 'score': 10},
+        {'text': 'Red', 'score': 5},
+        {'text': 'Green', 'score': 2},
+        {'text': 'White', 'score': 1},
+      ],
     },
     {
       'questionText': 'What\'s your favourite animal?',
-      'answers': ['Rabbit', 'Snake', 'Elephant', 'Lion']
+      'answers': [
+        {'text': 'Rabbit', 'score': 8},
+        {'text': 'Snake', 'score': 6},
+        {'text': 'Elephant', 'score': 3},
+        {'text': 'Lion', 'score': 4},
+      ],
     },
     {
       'questionText': 'Who\'s your favourite instructor?',
-      'answers': ['Andy', 'Andy', 'Andy', 'Andy']
+      'answers': [
+        {'text': 'Andy', 'score': 5},
+        {'text': 'Andy', 'score': 2},
+        {'text': 'Andy', 'score': 4},
+        {'text': 'Andy', 'score': 7},
+      ],
     },
   ];
 
-  void _answerQuestion() {
+  var _questionIndex = 0;
+  var _totalScore = 0;
+
+  void _resetQuiz() {
+    setState(() {
+      _questionIndex = 0;
+      _totalScore = 0;
+    });
+  }
+
+  void _answerQuestion(int score) {
+    _totalScore += score;
+
     setState(() {
       _questionIndex = _questionIndex + 1;
     });
+
     print(_questionIndex);
 
     if (_questionIndex < _questions.length) {
@@ -46,16 +72,17 @@ class _MyAppState extends State<MyApp> {
     //build method will return a widget
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(
-            title: Text('My First App'),
-          ),
-          body: _questionIndex < _questions.length
-              ? Quiz(
-                  questions: _questions,
-                  answerQuestion: _answerQuestion,
-                  questionIndex: _questionIndex,
-                )
-              : Result(),
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: _questionIndex < _questions.length
+            ? Quiz(
+                questions: _questions,
+                answerQuestion: _answerQuestion,
+                questionIndex: _questionIndex,
+              )
+            : Result(totalScore: _totalScore, resetQuiz: _resetQuiz),
+      ),
     );
   }
 }
